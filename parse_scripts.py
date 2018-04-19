c621e62c76f01ab789d47075e4791fcb5e6ec06e
goldstar611 <goldstar611@users.noreply.github.com>
2018-04-18 23:31:15 -0500

    open files

--- parse_scripts.py
+++ parse_scripts.py
@@ -1,15 +1,21 @@
+import os
 
 
-def parse_set_descriptor():
-    pass
+def parse_set_descriptor(set_number=1):
+    with open(os.path.join("set%i" % set_number, "scripts", "set.sdf")) as f:
+        pass
 
 
-def parse_visproto():
-    pass
+def parse_visproto(set_number=1):
+
+    with open(os.path.join("set%i" % set_number, "scripts", "visproto.lst")) as f:
+        pass
 
 
-def parse_slurps():
-    pass
+def parse_slurps(set_number=1):
+
+    with open(os.path.join("set%i" % set_number, "scripts", "slurps.lst")) as f:
+        pass
 
 
 if __name__ == "__main__":
