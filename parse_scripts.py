d91435057cee1f8b2f81e61eb4a4e352f3ef4a25
goldstar611 <goldstar611@users.noreply.github.com>
2018-04-18 23:28:51 -0500

    add parse scripts skeleton

---
+++ parse_scripts.py
@@ -0,0 +1,18 @@
+
+
+def parse_set_descriptor():
+    pass
+
+
+def parse_visproto():
+    pass
+
+
+def parse_slurps():
+    pass
+
+
+if __name__ == "__main__":
+    parse_set_descriptor()
+    parse_visproto()
+    parse_slurps()
