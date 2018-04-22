9093856df01498002a580bb3fceeb422ab2e0c7a
goldstar611 <goldstar611@users.noreply.github.com>
2018-04-22 10:20:53 -0500

    Converter for .bas to .json

---
+++ decompile_bas.py
@@ -0,0 +1,17 @@
+import compile
+import glob
+
+
+def main():
+    files = glob.glob("**/*.bas", recursive=True)
+
+    for file in files:
+        print("file:", file)
+        with open(file, "rb") as f:
+            form = compile.Form().load_from_file(file)
+            with open(file + ".json", "w") as o:
+                o.write(form.to_json())
+
+
+if __name__ == '__main__':
+    main()
