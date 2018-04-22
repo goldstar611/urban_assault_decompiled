19a1b55d51f2842f90d29f165c3c5075329b29bd
goldstar611 <goldstar611@users.noreply.github.com>
2018-04-21 22:06:52 -0500

    Update with code (file soon to be removed)

--- parse_scripts.py
+++ parse_scripts.py
@@ -2,23 +2,42 @@ import os
 
 
 def parse_set_descriptor(set_number=1):
-    with open(os.path.join("set%i" % set_number, "scripts", "set.sdf")) as f:
-        pass
+    set = []
+    with open(os.path.join("set%i" % set_number, "scripts", "set.sdf"), "rb") as f:
+        for line in f:
+            if b">" in line:
+                break
+            base = line.split(b" ")[0].decode("ascii")
+            set.append(base)
+    print("set:", set)
+    return set
 
 
 def parse_visproto(set_number=1):
-
-    with open(os.path.join("set%i" % set_number, "scripts", "visproto.lst")) as f:
-        pass
+    visproto = []
+    with open(os.path.join("set%i" % set_number, "scripts", "visproto.lst"), "rb") as f:
+        for line in f:
+            if b">" in line:
+                break
+            base = line.split(b";")[0].decode("ascii").strip()
+            visproto.append(base)
+    print("visproto:", visproto)
+    return visproto
 
 
 def parse_slurps(set_number=1):
-
-    with open(os.path.join("set%i" % set_number, "scripts", "slurps.lst")) as f:
-        pass
+    slurps = []
+    with open(os.path.join("set%i" % set_number, "scripts", "slurps.lst"), "rb") as f:
+        for line in f:
+            if b">" in line:
+                break
+            base = line.split(b";")[0].decode("ascii").strip()
+            slurps.append(base)
+    print("slurps:", slurps)
+    return slurps
 
 
 if __name__ == "__main__":
-    parse_set_descriptor()
-    parse_visproto()
-    parse_slurps()
+    parse_set_descriptor(1)
+    parse_visproto(1)
+    parse_slurps(1)
