f61eb63374b29f8ab623a2ad79117c91827da365
goldstar611 <goldstar611@users.noreply.github.com>
2018-05-09 09:52:19 -0500

    Add extract EMBD resources for later reference

--- decompile_bas.py
+++ decompile_bas.py
@@ -12,6 +12,13 @@ def main():
             with open(file + ".json", "w") as o:
                 o.write(form.to_json())
 
+        # Extract EMBD Resources
+        bas_file = compile.Mc2().load_from_file(file)
+        form = bas_file.get_single_form_by_type("EMBD")
+        embd = compile.Embd()
+        embd.sub_chunks = form.sub_chunks
+        embd.extract_resources("output/dontcare")
+
 
 if __name__ == '__main__':
     main()
