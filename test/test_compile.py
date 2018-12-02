e85ba0bb39201e99f9223f9562514e37b67e5142
goldstar611 <goldstar611@users.noreply.github.com>
2018-12-02 08:38:52 -0600

    No globals

--- test/test_compile.py
+++ test/test_compile.py
@@ -1,62 +1,78 @@
 import compile
 import unittest
 
-test_chunk_id = "TEST"
-data_bytes = b"This is bytes"
-data_string = "This is not bytes"
-c = compile.Chunk(test_chunk_id)
-
 
 class TestChunk(unittest.TestCase):
 
     def test_init(self):
+        test_chunk_id = "TEST"
+        c = compile.Chunk(test_chunk_id)
         self.assertEqual(c.chunk_id, test_chunk_id)
 
     def test_validated_id_1(self):
+        test_chunk_id = "TEST"
+        c = compile.Chunk(test_chunk_id)
         self.assertEqual(c.validate_id(test_chunk_id), test_chunk_id)
 
     def test_validated_id_2(self):
+        c = compile.Chunk()
         with self.assertRaises(ValueError) as context:
             c.validate_id(b"TEST")
         self.assertTrue("chunk_id must be a string" in str(context.exception))
 
     def test_validated_id_3(self):
+        c = compile.Chunk()
         with self.assertRaises(ValueError) as context:
             c.validate_id("TES")
         self.assertTrue("chunk_id must be 4" in str(context.exception))
 
     def test_validate_data_1(self):
+        c = compile.Chunk()
+        data_bytes = b"This is bytes"
         self.assertEqual(c.validate_data(data_bytes), data_bytes)
 
     def test_validate_data_2(self):
+        c = compile.Chunk()
+        data_string = "This is not bytes"
         with self.assertRaises(ValueError) as context:
             c.validate_data(data_string)
         self.assertTrue("chunk_data must be bytes" in str(context.exception))
 
     def test_set_binary_data(self):
+        test_chunk_id = "TEST"
+        c = compile.Chunk(test_chunk_id)
+        data_bytes = b"This is bytes"
         c.set_binary_data(data_bytes)
         self.assertEqual(c.get_data(), data_bytes)
 
     def test_full_data(self):
-        c.chunk_id = test_chunk_id
+        c = compile.Chunk()
+        data_bytes = b"This is bytes"
+        c.chunk_id = "TEST"
         c.set_binary_data(data_bytes)
         self.assertEqual(c.full_data(), b"TEST\x00\x00\x00\rThis is bytes\x00")
 
     def test_size(self):
+        c = compile.Chunk()
+        data_bytes = b"This is bytes"
         c.set_binary_data(data_bytes)
         self.assertEqual(c.size(), len(data_bytes))
 
     def test_load_data_from_file_1(self):
+        c = compile.Chunk()
         with self.assertRaises(FileNotFoundError) as context:
             c.load_data_from_file("a_file_not_exist")
         self.assertTrue("could not be found" in str(context.exception))
 
     def test_load_data_from_file_2(self):
+        c = compile.Chunk()
         with self.assertRaises(ValueError) as context:
             c.load_data_from_file("test/test_form.bin")
         self.assertTrue("FORM file into a chunk" in str(context.exception))
 
     def test_load_data_from_file_3(self):
+        c = compile.Chunk()
+        data_bytes = b"This is bytes"
         c.set_binary_data(data_bytes)
         c.load_data_from_file("test/test_chunk.bin")
         self.assertNotEqual(c.get_data(), data_bytes)
@@ -66,6 +82,7 @@ class TestChunk(unittest.TestCase):
     # TODO save_to_file
 
     def test_save_to_class(self):
+        c = compile.Chunk()
         c.chunk_id = "NAME"
         c.data = b"TEST\x00"
         cl = c.to_class()
@@ -74,6 +91,7 @@ class TestChunk(unittest.TestCase):
         self.assertEqual(cl.full_data(), b'NAME\x00\x00\x00\x05TEST\x00\x00')
 
     def test_to_json(self):
+        c = compile.Chunk()
         c.chunk_id = "NAME"
         c.data = b"TEST\x00"
         j = c.to_json()
