c57f54496c64c76e6d6a4bab07ae20d043f2765d
goldstar611 <goldstar611@users.noreply.github.com>
2018-12-06 20:06:04 -0600

    update work in progress

--- sky.py
+++ sky.py
@@ -4,7 +4,7 @@ import os
 from PyQt5 import QtGui
 
 
-def main():
+def decompile_sky():
     for sky in glob.glob("sky/*.bas"):
         sky_dir = os.path.splitext(sky)[0]
         form = compile.Form().load_from_file(sky)
@@ -20,5 +20,35 @@ def main():
                 f.write(ades.to_json())
 
 
+def compile_sky():
+    with open(os.path.join("sky", "sky_generic.json"), "rt") as f:
+        generic_sky_json = f.read()
+
+    for sky in glob.glob("sky/*"):
+        print(sky)
+
+        generic_sky = compile.Form().from_json(generic_sky_json)
+        embd = generic_sky.get_single_form_by_type("EMBD").to_class()  # to_class() makes this a new object. Not part of generic_sky object anymore
+        name = generic_sky.get_single_chunk_by_id("NAME").to_class()  # to_class() makes this a new object. Not part of generic_sky object anymore
+        sky_dir = os.path.basename(sky)
+
+        json_files = glob.glob(os.path.join(sky, "[!ades]*.json"))
+        bmp_files = glob.glob(os.path.join(sky, "*.bmp"))
+        ades_file = os.path.join(sky, "ades.json")
+
+        for bmp_file in bmp_files:
+            print(bmp_file)
+            vbmp = compile.Vbmp().load_from_bmp(bmp_file)
+            embd.add_vbmp(bmp_file, vbmp)
+        for json_file in json_files:
+            print(json_file)
+            with open(json_file, "rt") as f:
+                sklt = compile.Form().from_json(f.read())
+            embd.add_sklt(json_file, sklt)
+        if os.path.isfile(ades_file):
+            print(ades_file)
+
+
 if __name__ == '__main__':
-    main()
+    decompile_sky()
+    compile_sky()
