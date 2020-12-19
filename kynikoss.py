d0f0aabfdd8222f6a4f0b2e96d4913d9c8ec50f1
goldstar611 <goldstar611@users.noreply.github.com>
2020-12-19 11:12:34 -0600

    update before delete

--- kynikoss.py
+++ kynikoss.py
@@ -3,18 +3,20 @@ import os
 from PyQt5 import QtGui, QtCore
 
 
-sky_file = compile.Mc2().load_from_file(os.path.join("sky", "haamitt1.bas"))
-print(sky_file.to_json())
-image_dir = os.path.join("sky", "haamitt1")
+file_name = os.path.join("assets","sky", "haamitt1",  "sky.json")
+sky_file = compile.Mc2().from_json_file(file_name)
+image_dir = os.path.dirname(file_name)
 
 cibo_forms = sky_file.get_all("CIBO")
 
-for cibo_form in cibo_forms:
-    image_filename = cibo_form.get_single("NAM2").conversion_class.name
-    otl2_points = cibo_form.get_single("OTL2").conversion_class.points
+for i, cibo_form in enumerate(cibo_forms):
+    image_filename = cibo_form.get_single("NAM2").to_class().name
+    otl2_points = cibo_form.get_single("OTL2").to_class().points
     print(image_filename, otl2_points)
 
     image = QtGui.QImage(os.path.join(image_dir, image_filename + ".bmp"))
+    if image.format() == QtGui.QImage.Format_Invalid:
+        raise ValueError("image format was invalid!")
     image = image.convertToFormat(QtGui.QImage.Format_RGB32)
     painter = QtGui.QPainter(image)
     pen = QtGui.QPen()
@@ -24,4 +26,4 @@ for cibo_form in cibo_forms:
     painter.drawPolygon(*[QtCore.QPoint(x[0], x[1]) for x in otl2_points])
     painter.end()
 
-    image.save(os.path.join(image_dir, image_filename + ".bmp"))
+    image.save(os.path.join("output", image_filename + "{}.bmp".format(i)))
